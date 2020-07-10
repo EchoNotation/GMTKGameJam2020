790b7211d2de514dea7c6ceb3e967d39bdcc8919
Emilio Alvarez <e.l.alvarez@icloud.com>
2020-07-10 18:28:03 -0500

    GameControll Methods

---
+++ GMTKGameJam2020/Assets/Scripts/Gamecontroller.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamecontroller : MonoBehaviour
+{
+    private int gameScore;
+    public bool gamePaused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void IncrementScore() {
+
+    }
+
+    int GetScore() {
+        return gameScore;
+    }
+
+    void Reset() {
+        gameScore = 0;
+        Pause();
+    }
+
+    void Pause() {
+        gamePaused = true;
+    }
+
+    void Play() {
+        gamePaused = false;
+    }
+
+}

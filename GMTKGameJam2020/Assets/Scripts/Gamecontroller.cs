8d4b790d91cb036bff8b5b6462b16e9dedd7c41c
Emilio Alvarez <e.l.alvarez@icloud.com>
2020-07-11 18:15:08 -0500

    Variable is stuck?????

--- GMTKGameJam2020/Assets/Scripts/Gamecontroller.cs
+++ GMTKGameJam2020/Assets/Scripts/Gamecontroller.cs
@@ -11,21 +11,24 @@ public class Gamecontroller : MonoBehaviour
     private Camera mainCamera;
     public GameObject enemy, bombmer;
     private int[] waveEnemyMix;
-    private bool gamePaused, trickling;
+    public bool gamePaused, trickling;
 
-    void Awake() {
-        if (instance == null) {
+    void Awake()
+    {
+        if (instance == null)
+        {
             instance = this;
         }
-        else if (instance != this){
+        else if (instance != this)
+        {
             Destroy(this.gameObject);
         }
     }
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Booting GameController");
+        Debug.Log("Starting GameController!");
         DontDestroyOnLoad(this.gameObject);
         waveSpawnThreshold = 0.25f;
         enemiesActive = new ArrayList();
@@ -35,24 +38,38 @@ public class Gamecontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (mainCamera == null) {
-            if (GameObject.FindGameObjectWithTag("Player") != null) {
-                mainCamera = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Camera>();
-                halfWidth = mainCamera.orthographicSize * mainCamera.aspect;
+        if (gamePaused == false)
+        {
+            if (mainCamera == null)
+            {
+                Debug.Log("serching for camera");
+                if (GameObject.FindGameObjectWithTag("Player") != null)
+                {
+                    mainCamera = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Camera>();
+                    halfWidth = mainCamera.orthographicSize * mainCamera.aspect;
+                }
+            }
+            else
+            {
+                Debug.Log("Enemies: " + enemiesActive.Count);
+                if (enemiesActive.Count / waveEnemyCount < waveSpawnThreshold)
+                {
+                    SpawnNextWave();
+                }
+                if (!trickling)
+                {
+                    StartCoroutine(SpawnTrickle());
+                }
             }
         }
-        else if (!gamePaused) {
-            if (enemiesActive.Count / waveEnemyCount < waveSpawnThreshold) {
-                SpawnNextWave();
-            }
-            if (!trickling) {
-                StartCoroutine(SpawnTrickle());
-            }
-
+        else
+        {
+            Debug.Log("Game is Paused");
         }
     }
 
-    public void Reset() {
+    public void Reset()
+    {
         Pause();
         mainCamera = null;
         enemiesActive.Clear();
@@ -60,16 +77,19 @@ public class Gamecontroller : MonoBehaviour
         waveEnemyCount = 1;
         trickling = false;
     }
-    private IEnumerator SpawnTrickle() {
+    private IEnumerator SpawnTrickle()
+    {
         trickling = true;
-        yield return new WaitForSeconds(Random.Range(100,501) * 0.01f);
+        yield return new WaitForSeconds(Random.Range(100, 501) * 0.01f);
         Instantiate(enemy, SpawnLocation(), Quaternion.identity);
         waveEnemyCount = enemiesActive.Count / ((enemiesActive.Count - 1) / waveEnemyCount);
         trickling = false;
     }
-    private void SpawnNextWave() {
+    private void SpawnNextWave()
+    {
         waveEnemyCount = 0;
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++)
+        {
             waveEnemyCount++;
             Instantiate(enemy, SpawnLocation(), Quaternion.identity);
         }
@@ -78,11 +98,13 @@ public class Gamecontroller : MonoBehaviour
         Debug.Log("Wave " + waveNumber + " Spawned");
     }
 
-    private Vector2 SpawnLocation() {
+    private Vector2 SpawnLocation()
+    {
         //TODO: validate Coords on map, change distribution?
 
         Vector2 directionVector = Vector2.zero;
-        while (directionVector == Vector2.zero) {
+        while (directionVector == Vector2.zero)
+        {
             directionVector = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
         }
 
@@ -92,21 +114,27 @@ public class Gamecontroller : MonoBehaviour
         return directionVector * magnitude;
     }
 
-    public void RegisterEnemy(GameObject newEnemy) {
+    public void RegisterEnemy(GameObject newEnemy)
+    {
         enemiesActive.Add(newEnemy);
     }
-    public void RemoveEnemy(GameObject deadEnemy) {
+    public void RemoveEnemy(GameObject deadEnemy)
+    {
         enemiesActive.Remove(deadEnemy);
     }
 
-    public void Pause() {
+    public void Pause()
+    {
         gamePaused = true;
+        Debug.Log("Paused " + gamePaused);
         StopAllCoroutines();
         trickling = false;
     }
 
-    public void Play() {
+    public void Play()
+    {
         gamePaused = false;
+        Debug.Log("Playing " + gamePaused);
     }
 
     public void SpawnEnemy(Enemies type, Vector3 position)

96d85e60ddf4d7e32bc2dcd1aa0a8faad0bc79ed
Zack Johnson <zackmj99@gmail.com>
2020-07-11 17:16:18 -0500

    added spawn method
    needs "enemy" reference and calls where necesary

--- GMTKGameJam2020/Assets/Scripts/Gamecontroller.cs
+++ GMTKGameJam2020/Assets/Scripts/Gamecontroller.cs
@@ -96,4 +96,9 @@ public class Gamecontroller : MonoBehaviour
         gamePaused = false;
     }
 
+    public void SpawnEnemy(Enemies type, Vector3 position)
+    {
+        GameObject newEnemy = Instantiate(enemy, position, Quaternion.identity);
+        newEnemy.GetComponent<Enemy>().enemyType = type;
+    }
 }
